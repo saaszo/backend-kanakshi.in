22a30d4c6cd3ece225ca568c939d602715f107b2
saaszoin-glitch <saaszo.in@gmail.com>
2026-05-26 13:59:05 +0530

    Fix OTP verification setting casts

--- api/app/Models/OtpVerificationSetting.php
+++ api/app/Models/OtpVerificationSetting.php
@@ -9,4 +9,18 @@ class OtpVerificationSetting extends Model
     protected $table = 'otp_verification_settings';
 
     protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verification_enabled' => 'boolean',
+            'mobile_verification_enabled' => 'boolean',
+            'email_otp_enabled' => 'boolean',
+            'sms_otp_enabled' => 'boolean',
+            'whatsapp_otp_enabled' => 'boolean',
+            'otp_length' => 'integer',
+            'otp_expiry_minutes' => 'integer',
+            'resend_wait_seconds' => 'integer',
+        ];
+    }
 }
